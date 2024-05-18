9fa14c0465c4f7a4399a9d6eb4ca7c4220c92aab
Alex <alex.taranenko.job@gmail.com>
2024-05-18 19:02:36 +0300

    fix/catalog fixes

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,7 +1,22 @@
-import './productCard.scss';
+import styles from './productCard.module.scss';
 
-export const ProductCard = () => {
+type Props = {
+  name: string,
+  price: string,
+  url: string
+}
+
+export const ProductCard: React.FC<Props> = ({ name, price, url }) => {
   return (
-    <p> We are the best </p>
+    <li>
+      <img src={url} alt="item_image" />
+      <div className={styles.container_text}>
+        <p className={styles.title_poduct}>{name}</p>
+        <p className={styles.price_poduct}>{price}</p>
+      </div>
+      <a href="#prodact-view">
+        Переглянути
+      </a>
+    </li>
   )
 };
--- src/pages/productCatalog/ProductCatalog.tsx
+++ src/pages/productCatalog/ProductCatalog.tsx
@@ -1,12 +1,20 @@
-import "./productCatalog.scss";
-import css from "./ProductCatalog.module.css";
-import { useState } from "react";
-export const ProductCatalog = () => {
-  const [descriptionToogle, setDescriptionToogle] = useState(false);
-  const [amountProduct, setAmountProduct] = useState(1);
-  const handleClick = () => {
-    setDescriptionToogle(true);
-  };
+import { ProductCard } from "../../components/productCard/ProductCard";
+import css from "./ProductCatalog.module.scss";
+
+const cakses = [
+  {id: 1, name: "Наполеон", url: "src/pages/productCatalog/images/Frame56.jpg", price: "500 UAH"},
+  {id: 1, name: "Наполеон", url: "src/pages/productCatalog/images/Frame56.jpg", price: "500 UAH"},
+  {id: 1, name: "Наполеон", url: "src/pages/productCatalog/images/Frame56.jpg", price: "500 UAH"},
+  {id: 1, name: "Наполеон", url: "src/pages/productCatalog/images/Frame56.jpg", price: "500 UAH"},
+  {id: 1, name: "Наполеон", url: "src/pages/productCatalog/images/Frame56.jpg", price: "500 UAH"},
+  {id: 1, name: "Наполеон", url: "src/pages/productCatalog/images/Frame56.jpg", price: "500 UAH"},
+  {id: 1, name: "Наполеон", url: "src/pages/productCatalog/images/Frame56.jpg", price: "500 UAH"},
+  {id: 1, name: "Наполеон", url: "src/pages/productCatalog/images/Frame56.jpg", price: "500 UAH"},
+  {id: 1, name: "Наполеон", url: "src/pages/productCatalog/images/Frame56.jpg", price: "500 UAH"},
+  {id: 1, name: "Наполеон", url: "src/pages/productCatalog/images/Frame56.jpg", price: "500 UAH"}
+]
+
+export const ProductCatalog: React.FC = () => {
   return (
     <>
       <div className={`${css.container} ${css.container_product_catalog}`}>
@@ -51,213 +59,12 @@ export const ProductCatalog = () => {
         </div>
       </div>
 
-      {descriptionToogle && (
-        <div className={css.container} id="prodact-view">
-          <div
-            className={`${css.container_section_list} ${css.container_product_view}`}
-          >
-            <div className={css.container_img}>
-              <img src="src/pages/productCatalog/images/photo.jpg" alt="" />
-              <img src="src/pages/productCatalog/images/photo (1).jpg" alt="" />
-              <img src="src/pages/productCatalog/images/photo (2).jpg" alt="" />
-            </div>
-            <div className={css.container_form}>
-              <form action="" className={css.form}>
-                <p className={css.title_product_view}>Червоний оксамит</p>
-                <p className={css.price_product_view}>500 UAH</p>
-
-                <select name="select" required className={css.form_select}>
-                  <option disabled selected>
-                    Варіанти доставки
-                  </option>
-                  <option value="value1">Значення 1</option>
-                  <option value="value2">Значення 2</option>
-                  <option value="value3">Значення 3</option>
-                </select>
-                <select name="select" required className={css.form_select}>
-                  <option disabled selected>
-                    Діаметр
-                  </option>
-                  <option value="value1">Значення 1</option>
-                  <option value="value2">Значення 2</option>
-                  <option value="value3">Значення 3</option>
-                </select>
-                <select name="select" className={css.form_select}>
-                  <option disabled selected>
-                    Інгрідієнти
-                  </option>
-                  <option value="value1">Значення 1</option>
-                  <option value="value2">Значення 2</option>
-                  <option value="value3">Значення 3</option>
-                </select>
-                <select name="select" className={css.form_select}>
-                  <option disabled selected>
-                    Алергени
-                  </option>
-                  <option value="value1">Значення 1</option>
-                  <option value="value2">Значення 2</option>
-                  <option value="value3">Значення 3</option>
-                </select>
-                <select name="select" required className={css.form_select}>
-                  <option disabled selected>
-                    Доставка
-                  </option>
-                  <option value="value1">Значення 1</option>
-                  <option value="value2">Значення 2</option>
-                  <option value="value3">Значення 3</option>
-                </select>
-                <div className={css.container_btn}>
-                  <button
-                    onClick={() => {
-                      if (amountProduct === 1) {
-                        return;
-                      }
-                      setAmountProduct(amountProduct - 1);
-                    }}
-                  >
-                    -
-                  </button>
-                  <p>{amountProduct}</p>
-                  <button
-                    onClick={() => {
-                      setAmountProduct(amountProduct + 1);
-                    }}
-                  >
-                    +
-                  </button>
-                </div>
-                <button type="submit" className={css.btn_submit}>
-                  Додати у кошик
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-      )}
-
       <div className={css.container}>
         <div className={css.container_section_list}>
           <ul className={css.product_list}>
-            <li>
-              <img src="src/pages/productCatalog/images/Frame56.jpg" alt="" />
-              <div className={css.container_text}>
-                <p className={css.title_poduct}>Наполеон</p>
-                <p className={css.price_poduct}>500 UAH</p>
-              </div>
-              <a href="#prodact-view" onClick={handleClick}>
-                Переглянути
-              </a>
-            </li>
-            <li>
-              <img src="src/pages/productCatalog/images/Frame56.jpg" alt="" />
-              <div className={css.container_text}>
-                <p className={css.title_poduct}>Наполеон</p>
-                <p className={css.price_poduct}>500 UAH</p>
-              </div>
-              <a href="#prodact-view" onClick={handleClick}>
-                Переглянути
-              </a>
-            </li>
-            <li>
-              <img src="src/pages/productCatalog/images/Frame56.jpg" alt="" />
-              <div className={css.container_text}>
-                <p className={css.title_poduct}>Наполеон</p>
-                <p className={css.price_poduct}>500 UAH</p>
-              </div>
-              <a href="#prodact-view" onClick={handleClick}>
-                Переглянути
-              </a>
-            </li>
-            <li>
-              <img src="src/pages/productCatalog/images/Frame56.jpg" alt="" />
-              <div className={css.container_text}>
-                <p className={css.title_poduct}>Наполеон</p>
-                <p className={css.price_poduct}>500 UAH</p>
-              </div>
-              <a href="#prodact-view" onClick={handleClick}>
-                Переглянути
-              </a>
-            </li>
-            <li>
-              <img src="src/pages/productCatalog/images/Frame56.jpg" alt="" />
-              <div className={css.container_text}>
-                <p className={css.title_poduct}>Наполеон</p>
-                <p className={css.price_poduct}>500 UAH</p>
-              </div>
-              <a href="#prodact-view" onClick={handleClick}>
-                Переглянути
-              </a>
-            </li>
-            <li>
-              <img src="src/pages/productCatalog/images/Frame56.jpg" alt="" />
-              <div className={css.container_text}>
-                <p className={css.title_poduct}>Наполеон</p>
-                <p className={css.price_poduct}>500 UAH</p>
-              </div>
-              <a href="#prodact-view" onClick={handleClick}>
-                Переглянути
-              </a>
-            </li>
-            <li>
-              <img src="src/pages/productCatalog/images/Frame56.jpg" alt="" />
-              <div className={css.container_text}>
-                <p className={css.title_poduct}>Наполеон</p>
-                <p className={css.price_poduct}>500 UAH</p>
-              </div>
-              <a href="#prodact-view" onClick={handleClick}>
-                Переглянути
-              </a>
-            </li>
-            <li>
-              <img src="src/pages/productCatalog/images/Frame56.jpg" alt="" />
-              <div className={css.container_text}>
-                <p className={css.title_poduct}>Наполеон</p>
-                <p className={css.price_poduct}>500 UAH</p>
-              </div>
-              <a href="#prodact-view" onClick={handleClick}>
-                Переглянути
-              </a>
-            </li>
-            <li>
-              <img src="src/pages/productCatalog/images/Frame56.jpg" alt="" />
-              <div className={css.container_text}>
-                <p className={css.title_poduct}>Наполеон</p>
-                <p className={css.price_poduct}>500 UAH</p>
-              </div>
-              <a href="#prodact-view" onClick={handleClick}>
-                Переглянути
-              </a>
-            </li>
-            <li>
-              <img src="src/pages/productCatalog/images/Frame56.jpg" alt="" />
-              <div className={css.container_text}>
-                <p className={css.title_poduct}>Наполеон</p>
-                <p className={css.price_poduct}>500 UAH</p>
-              </div>
-              <a href="#prodact-view" onClick={handleClick}>
-                Переглянути
-              </a>
-            </li>
-            <li>
-              <img src="src/pages/productCatalog/images/Frame56.jpg" alt="" />
-              <div className={css.container_text}>
-                <p className={css.title_poduct}>Наполеон</p>
-                <p className={css.price_poduct}>500 UAH</p>
-              </div>
-              <a href="#prodact-view" onClick={handleClick}>
-                Переглянути
-              </a>
-            </li>
-            <li>
-              <img src="src/pages/productCatalog/images/Frame56.jpg" alt="" />
-              <div className={css.container_text}>
-                <p className={css.title_poduct}>Наполеон</p>
-                <p className={css.price_poduct}>500 UAH</p>
-              </div>
-              <a href="#prodact-view" onClick={handleClick}>
-                Переглянути
-              </a>
-            </li>
+            {cakses.map(cake => 
+              <ProductCard key={cake.id} name={cake.name} price={cake.price} url={cake.url} />
+            )}
           </ul>
         </div>
       </div>
