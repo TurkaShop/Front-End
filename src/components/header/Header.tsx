a148b83638383f63ccb87c085b0477fff067c7af
ZakZanko <zakzanko2112@gmail.com>
2024-05-18 19:15:31 +0300

    feature/main_page

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,45 @@
-import './header.scss';
+// import './header.scss';
+import css from "./header.module.scss";
 
 export const Header = () => {
   return (
-    <p> We are the best </p>
-  )
+    <header className={css.header__container}>
+      <div className={css.logo__img}>
+        <a href="/">
+          <img src="src/images/header/logo.svg" alt="logo" />
+        </a>
+      </div>
+      <ul className={css.header__list}>
+        <li className={css.header__text_item}>
+          <a href="/">Торти</a>
+        </li>
+        <li className={css.header__text_item}>
+          <a href="/">Випічка</a>
+        </li>
+        <li className={css.header__text_item}>
+          <a href="/">Десерти</a>
+        </li>
+        <li className={css.header__text_item}>
+          <a href="/">Контакти</a>
+        </li>
+      </ul>
+      <ul className={css.header__icons}>
+        <li className={css.header__icons_item}>
+          <a href="/">
+            <img src="src/images/header/gg_search.svg" />
+          </a>
+        </li>
+        <li className={css.header__icons_item}>
+          <a href="/">
+            <img src="src/images/header/lets-icons_basket-alt-3-light.svg" />
+          </a>
+        </li>
+        <li className={css.header__icons_item}>
+          <a href="/">
+            <img src="src/images/header/material-symbols_person-outline.svg" />
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
 };
--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,7 +1,92 @@
-import './mainPage.scss';
+// import "./mainPage.scss";
+import css from "./mainPage.module.scss";
 
-export const MainPage = () => {
+export const MainPage: React.FC = () => {
   return (
-    <p> We are the best </p>
-  )
+    <main>
+      <section className={`${css.hero} ${css.container}`}>
+        <div className={css.hero__content}>
+          <div className={css.hero__text}>
+            <p className={css.welcome__text}>
+              привіт, <p>мій солоденький</p>
+            </p>
+            <p className={css.hero__btn_container}>
+              <a href="/">
+                <button className={css.hero__btn}>Спробувати найкраще</button>
+              </a>
+            </p>
+          </div>
+          <ul className={css.slider}>
+            <li className={css.dot__items}></li>
+            <li className={css.dot__items}></li>
+            <li className={css.dot__items}></li>
+          </ul>
+          <div className={css.icons__links}>
+            <a href="/">
+              <img
+                src="src/images/main_page/icons/basil_viber-solid.svg"
+                alt="viber"
+                className={css.icon__item}
+              />
+            </a>
+            <a href="/">
+              <img
+                src="src/images/main_page/icons/bi_instagram.svg"
+                alt="instagram"
+                className={css.icon__item}
+              />
+            </a>
+            <a href="/">
+              <img
+                src="src/images/main_page/icons/simple-icons_telegram.svg"
+                alt="telegram"
+                className={css.icon__item}
+              />
+            </a>
+          </div>
+        </div>
+      </section>
+      <section className={css.our__goods}>
+        <p className={css.main__text}>Наші товари</p>
+        <p className={css.second__text}>
+          Вже більше 10 років Tyrka Bakery робить вашу улюблену випічку з нуля,
+          невеликими партіями та з використанням найкращих інгредієнтів.
+        </p>
+        <button className={css.goods__btn}>
+          <p className={css.third__text}>Дивитись більше</p>
+        </button>
+      </section>
+      <section className={css.categories}>
+        <ul className={css.categories__list}>
+          <li className={css.categories__item}>
+            <img
+              src="src/images/main_page/categories/cakes.png"
+              alt="cakes"
+              className={css.categories__img}
+            />
+            <p className={css.categories__item_text}>Торти</p>
+          </li>
+          <li className={css.categories__item}>
+            <img
+              src="src/images/main_page/categories/baking.png"
+              alt="Baking"
+              className={css.categories__img}
+            />
+            <p className={css.categories__item_text}>Випічка</p>
+          </li>
+          <li className={css.categories__item}>
+            <img
+              src="src/images/main_page/categories/desserts.png"
+              alt="Desserts"
+              className={css.categories__img}
+            />
+            <p className={css.categories__item_text}>Десерти</p>
+          </li>
+        </ul>
+      </section>
+      <section className={css.our_desserts}>
+        <p className={css.main__text}>Наші десерти</p>
+      </section>
+    </main>
+  );
 };
